c456a8d8d67aa807c15048b095dd930cb40f76f3
Jeff Steele <steelem81@gmail.com>
2022-10-15 16:09:23 -0600

    Added path variables

--- src/main/java/com/jeffs/daikichi_routes/controllers/DaikichiController.java
+++ src/main/java/com/jeffs/daikichi_routes/controllers/DaikichiController.java
@@ -1,12 +1,14 @@
 package com.jeffs.daikichi_routes.controllers;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/daikichi")
 public class DaikichiController {
 	
-	@RequestMapping("/")
+	@RequestMapping("")
 	public String index() {
 		return "Welcome";
 	}
@@ -19,6 +21,21 @@ public class DaikichiController {
 	@RequestMapping("/tomorrow")
 	public String tomorrow() {
 		return "Tomorrow, an opportunity will arise, so be sure to be open to new ideas!";
+	}
+	
+	@RequestMapping("/travel/{city}")
+	public String travel(@PathVariable("city") String city) {
+		return "Congratulations! You will soon travel to " + city;	}
+	
+	@RequestMapping("/lotto/{luckyNum}")
+	public String lotto(@PathVariable("luckyNum") int luckyNum) {
+		String lottoMessage;
+		if (luckyNum % 2 == 0) {
+			lottoMessage = "You will take a grand jounrey in the near future, but be weary of tempting offers.";
+		} else {
+			lottoMessage = "You have enjoyed the fruits of your labor but now is a great time to spend with family and friends.";
+		}
+		return lottoMessage;
 	}
 
 }
